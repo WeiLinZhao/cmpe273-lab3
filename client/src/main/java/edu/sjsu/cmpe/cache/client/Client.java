832f3a99a67c394dac7b6221a699329409815e77
Wei Lin Zhao <winniewlz@gmail.com>
2014-11-12 23:27:41 -0800

    final

--- client/src/main/java/edu/sjsu/cmpe/cache/client/Client.java
+++ client/src/main/java/edu/sjsu/cmpe/cache/client/Client.java
@@ -9,12 +9,15 @@ public class Client {
         System.out.println("Starting Cache Client...");
         char[] alphabet = "abcdefghij".toCharArray();
         ArrayList<CacheServiceInterface> array = new ArrayList<CacheServiceInterface>();
-        array.add(new DistributedCacheService(
-                "http://localhost:3000"));
-        array.add(new DistributedCacheService(
-                "http://localhost:3001"));
-        array.add(new DistributedCacheService(
-                "http://localhost:3002"));
+        CacheServiceInterface serverA = new DistributedCacheService(
+                "http://localhost:3000");
+        CacheServiceInterface serverB = new DistributedCacheService(
+                "http://localhost:3001");
+        CacheServiceInterface serverC = new DistributedCacheService(
+                "http://localhost:3002");
+        array.add(serverA);
+        array.add(serverB);
+        array.add(serverC);
 
 
         ConsistentHash consistenthash = new ConsistentHash(5, array);
@@ -24,6 +27,15 @@ public class Client {
         for (int i = 1; i<11;i++){
             server = consistenthash.get(i);
             m = (CacheServiceInterface)server;
+            if (m == serverA){
+                System.out.print("http://localhost:3000  ");
+            }
+            if (m == serverB){
+                System.out.print("http://localhost:3001  ");
+            }
+            if (m == serverC){
+                System.out.print("http://localhost:3002  ");
+            }
             m.put(i,String.valueOf(alphabet[i-1]));
             System.out.println("put("+ String.valueOf(i)+" => " + String.valueOf(alphabet[i-1]) + ")");
         }
@@ -32,6 +44,16 @@ public class Client {
         for (int i = 1; i<11;i++){
             server = consistenthash.get(i);
             m = (CacheServiceInterface)server;
+            if (m == serverA){
+                System.out.print("http://localhost:3000  ");
+            }
+            if (m == serverB){
+                System.out.print("http://localhost:3001  ");
+            }
+            if (m == serverC){
+                System.out.print("http://localhost:3002  ");
+            }
+            
             System.out.println("get(" + String.valueOf(i)+ ") => " + m.get(i));
         }
 
